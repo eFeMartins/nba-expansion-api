dd6919ec83e4f430952e5c379bd330113df2ef60
Érick Martins <erickfemartins1230@gmail.com>
2026-08-30 15:17:46 -0300

    initial setup springboot 2.7 project

---
+++ src/main/java/com/example/demo/NbaExpansionApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaExpansionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaExpansionApiApplication.class, args);
+	}
+
+}
